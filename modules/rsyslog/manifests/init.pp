d906c39d1af6030f9dbdd0dc73166454f431bda3
Marcelo F Andrade <mfandrade@gmail.com>
2014-11-13 15:52:21 -0300

    Prossegue na configuração do rsyslog

--- modules/rsyslog/manifests/init.pp
+++ modules/rsyslog/manifests/init.pp
@@ -9,7 +9,7 @@ class rsyslog {
 
   file { 'rsyslog.conf':
     path    => '/etc/ryslog.conf',
-    content => 'puppet:///modules/rsyslog/rsyslog.conf',
+    source  => 'puppet:///modules/rsyslog/rsyslog.conf',
     require => Package['rsyslog-relp'],
   }
 
@@ -19,6 +19,31 @@ class rsyslog {
     hasrestart => true,
     hasstatus  => true,
     require    => File['rsyslog.conf'],
+  }
+
+  file { 'relp.conf':
+    ensure => present,
+    path   => '/etc/rsyslog.d/relp.conf',
+    owner  => root,
+    group  => root,
+    mode   => '0644',
+    source => 'puppet:///modules/rsyslog/relp.conf',
+  }
+  file { 'mysql.conf':
+    ensure  => present,
+    path    => '/etc/rsyslog.d/mysql.conf',
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    source  => 'puppet:///modules/rsyslog/mysql.conf',
+    require => Package['rsyslog-mysql'],
+  }
+  file { '/var/run/rsyslog':
+    ensure  => directory,
+    owner   => root,
+    group   => root,
+    mode    => '0755',
+    require => File['mysql.conf'],
   }
 
 }
