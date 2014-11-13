6f703c47d8f3bc065b8101be1c4c778b2c1647d5
Marcelo F Andrade <mfandrade@gmail.com>
2014-11-13 13:57:16 -0300

    Controla serviço e senha default

--- modules/mysql/manifests/init.pp
+++ modules/mysql/manifests/init.pp
@@ -1,7 +1,22 @@
-class mysql {
+class mysql ($root_passwd = 'root') {
 
   package { 'mysql-server':
     ensure   => installed,
+  }
+
+  service { 'mysql':
+    ensure     => running,
+    enable     => true,
+    hasrestart => true,
+    hasstatus  => true,
+    require    => Package['mysql-server'],
+  }
+
+  exec { 'Set password':
+    command => "mysqladmin -uroot password ${root_passwd}",
+    unless  => "mysqladmin -uroot -p${root_passwd} status",
+    path    => '/usr/bin',
+    require => Service['mysql'],
   }
 
 }
