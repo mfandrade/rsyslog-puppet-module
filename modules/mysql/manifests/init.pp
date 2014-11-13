c6582f6ce70ee9a72e7a32cb896a911ecfc3ad4c
Marcelo F Andrade <mfandrade@gmail.com>
2014-11-13 10:57:53 -0300

    Mirror no sources.list

--- modules/mysql/manifests/init.pp
+++ modules/mysql/manifests/init.pp
@@ -1,7 +1,13 @@
 class mysql {
 
+  exec { 'Prepare':
+    command => 'sed -i "s/us\./br./" /etc/apt/sources.list; apt-get update -y',
+    path    => '/bin:/usr/bin',
+  }
+
   package { 'mysql-server':
-    ensure => latest,
+    ensure  => installed,
+    require => Exec['Prepare'],
   }
 
 }
